4a48a423f90bf5834db30942082fd8c0fda41b96
LizHitches <liz.hitches@gmail.com>
2019-08-26 20:23:36 -0400

    adding classes

---
+++ BreakfastClassApp.cs
@@ -0,0 +1,33 @@
+﻿
+using System;
+using System.Collections.Generic;
+​
+namespace BreakfastClub
+{
+    class BreakfastClubApp
+    {
+        List<string> BreakfastMenu = new List<string>();
+​
+​
+        public BreakfastClubApp()
+        {
+            Console.WriteLine("Welcome to the Breakfast Club.");
+​
+        }
+​
+        public void GetOrder()
+        {
+​
+        }
+​
+        public void PrintMenu()
+        {
+​
+        }
+​
+        public void PaymentProcess()
+        {
+​
+        }
+    }
+}
