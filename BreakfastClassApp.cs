9f96ac936247384b53b4b1ec5429725da30cb89b
maniknath <maniknath79@gmail.co>
2019-08-26 21:17:04 -0400

    new commit

--- BreakfastClassApp.cs
+++ BreakfastClassApp.cs
@@ -1,33 +1,34 @@
 ﻿
 using System;
 using System.Collections.Generic;
-​
+
 namespace BreakfastClub
 {
     class BreakfastClubApp
     {
-        List<string> BreakfastMenu = new List<string>();
-​
-​
+        List<Menu> BreakfastMenu = new List<Menu>();
+
+
         public BreakfastClubApp()
         {
             Console.WriteLine("Welcome to the Breakfast Club.");
-​
+
+
         }
-​
+
         public void GetOrder()
         {
-​
+
         }
-​
+
         public void PrintMenu()
         {
-​
+
         }
-​
+
         public void PaymentProcess()
         {
-​
+
         }
     }
 }
