1ee5b7922d7ee5f2151fa3cb2e1e820ed5269cdc
LizHitches <50306247+LizHitches@users.noreply.github.com>
2019-09-02 23:01:29 -0400

    Update Orders.cs

--- Orders.cs
+++ Orders.cs
@@ -1,53 +1,62 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Linq;
+using System.Globalization;
 
 namespace BreakfastClub
-
 {
     public class Order
     {
         List<Menu> menus = Menu.MenuItems();
         List<MenuItem> cart = new List<MenuItem>();
 
-
         public Order()
         {
-        }
-
-        public List<MenuItem> AddMenuItem(int input)
-        {
-            List<MenuItem> cart = new List<MenuItem>();
-
-            cart.Add(new MenuItem { OrderName = menus[input].Name, OrderPrice = menus[input].Price });
-
-            return cart;
         }
         public void PrintMenu()
         {
             int i = 0;
+
             foreach (var n in menus)
             {
                 i++;
-                Console.WriteLine(i + "." + n.Name + " \t " + n.Price);
+                Console.WriteLine(i + ". " + "{0,1} {1,20} {2,15}", n.Name, n.Category, n.Price);
+            }
+            Console.WriteLine(menus.Count + 1 + ". End Order and Procede to payment");
+        }
+        public List<MenuItem> AddMenuItem(int input, int howMany)
+        {
+            for (int i = 0; i < howMany; i++)
+            {
+                cart.Add(new MenuItem { OrderName = menus[input].Name, OrderPrice = menus[input].Price });
+            }
+            return cart;
+        }
+        public void PrintCart()
+        {
+            double total = cart.Sum(cart => cart.OrderPrice);
 
+            Console.WriteLine("\n Cart Total: " + total.ToString("C2", CultureInfo.CurrentCulture));
+            Console.WriteLine("________________________________");
+            int i = 0;
+            foreach (var item in cart)
+            {
+                i++;
+                Console.WriteLine(i + "." + item.OrderName + "\t" + item.OrderPrice);
             }
         }
+        public double GetTotal()
+        {
+
+            double Total = cart.Sum(cart => cart.OrderPrice);
+
+            return Total;
+
+        }
+
+
     }
-
-    internal class Payment
-    {
-        private Menu menu;
-
-    }
-
-    public class MenuItem
-    {
-        internal object OrderName;
-        internal object OrderPrice;
-    }
-
-
 
 }
 
