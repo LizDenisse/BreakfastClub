1f3aeedda519952fad8fe660fc89511efb424132
LizHitches <50306247+LizHitches@users.noreply.github.com>
2019-09-04 20:20:30 -0400

    Update Orders.cs

--- Orders.cs
+++ Orders.cs
@@ -1,36 +1,69 @@
-﻿    using System;
-    using System.Collections.Generic;
-    using System.Text;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Globalization;
 
 namespace BreakfastClub
-
+{
+    public class Order
     {
-        public class Order
-        {
+        public string OrderName { get; set; }
+        public double OrderPrice { get; set; }
+
+
         List<Menu> menus = Menu.MenuItems();
-        List<MenuItem> items = new List<MenuItem>();
-            public Order()
-            {
-                Console.WriteLine("Hello User! Welcome to the Breakfast Club! Home of the best burgers in town.");
-                Console.WriteLine("Have a look at our menu!");
-            }
-            public MenuItem AddMenuItem()
+        public List<Order> cart = new List<Order>();
+        public void PrintMenu()
         {
-                menus.Add(items);
+            int i = 0;
 
-                return item;
-            }
-            public void PrintMenu()
+            foreach (var n in menus)
             {
-                int i = 0;
-                     foreach (var n in menus)
-                {
-                    Console.WriteLine(i++ + ". " + n.ToString());
-                }
+                i++;
+                Console.WriteLine(i + ". " + n.Name + "\t| " + n.Category + " \t |" + n.Price);
             }
+            Console.WriteLine(menus.Count + 1 + ". End Order and Procede to payment");
+        }
+        public List<Order> AddMenuItem(int input, int howMany)
+        {
+            for (int i = 0; i < howMany; i++)
+            {
+                cart.Add(new Order { OrderName = menus[input].Name, OrderPrice = menus[input].Price });
+            }
+            return cart;
         }
 
-       public class MenuItem
+        public double GetGrandTotal()
         {
+            double SubTotal = cart.Sum(x => x.OrderPrice);
+            double SalesTax = SubTotal * 0.07;
+
+            double grandTotal = SubTotal + SalesTax;
+
+            return grandTotal;
+
         }
+        public void PrintCart()
+        {
+            double SubTotal = cart.Sum(cart => cart.OrderPrice);
+            double SalesTax = SubTotal * 0.07;
+            double grandTotal = SubTotal + SalesTax;
+
+            int i = 0;
+
+            Console.WriteLine("Cart:");
+            foreach (Order item in cart)
+            {
+                i++;
+                Console.WriteLine(i + "." + item.OrderName + "\t " + item.OrderPrice);
+            }
+            Console.WriteLine("________________________________");
+            Console.WriteLine("\n Sub Total: " + SubTotal.ToString("C2", CultureInfo.CurrentCulture));
+            Console.WriteLine("\n Tax: " + SalesTax.ToString("C2", CultureInfo.CurrentCulture));
+            Console.WriteLine("\n Grand Total: " + grandTotal.ToString("C2", CultureInfo.CurrentCulture));
+            Console.WriteLine("________________________________");
+        }
+    }
 }
+
