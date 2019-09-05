75e308f20c31a252a2dc10942319eff7814c308d
LizHitches <50306247+LizHitches@users.noreply.github.com>
2019-09-05 17:07:30 -0400

    Update Menu.cs

--- Menu.cs
+++ Menu.cs
@@ -1,109 +1,64 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
-namespace BreakfastClubMyVersion
+namespace BreakfastClub
 {
     public class Menu
     {
-
         public string Name { get; set; }
         public string Category { get; set; }
         public string Description { get; set; }
-        public double price { get; set; }
+        public double Price { get; set; }
 
+        public Menu(string name, string category, string description, double price)
+        {
+            this.Name = name;
+            this.Category = category;
+            this.Description = description;
+            this.Price = price;
+        }
         static public List<Menu> MenuItems()
         {
-            Menu item1 = new Menu()
-            {
-                Name = "Awesome Avocado ",
-                Category = "Omellet",
-                Description = "Stuffed with ripe avocado, lean turnkey bacon, bell pepper, a blend of Monterey Jack and Cheddar cheeses. Served with our homemade salsa.",
-                price = 13.50
-            };
 
-            Menu Item2 = new Menu()
-            {
-                Name = "VEGGIE DELIGHT ",
-                Category = "",
-                Description = "",
-                price = 10.30
-            };
-            Menu Item3 = new Menu()
-            {
-                Name = "LEAN & GREEN",
-                Category = "",
-                Description = "",
-                price = 13.00
-            };
-            Menu Item4 = new Menu()
-            {
-                Name = "CHICKEN FAJITA ",
-                Category = "",
-                Description = "",
-                price = 0
-            };
-            Menu Item5 = new Menu()
-            {
-                Name = "JUMP START BREAKFAST SANDWICH ",
-                Category = "",
-                Description = "",
-                price = 0
-            };
-            Menu Item6 = new Menu()
-            {
-                Name = "The Helthier English ",
-                Category = "",
-                Description = "",
-                price = 0
-            };
-            Menu Item7 = new Menu()
-            {
-                Name = "The Happy American ",
-                Category = "",
-                Description = "",
-                price = 0
-            };
-            Menu Item8 = new Menu()
-            {
-                Name = "Pancakes ",
-                Category = "",
-                Description = "",
-                price = 0
-            };
-            Menu Item9 = new Menu()
-            {
-                Name = "Parfait ",
-                Category = "",
-                Description = "",
-                price = 0
-            };
-            Menu Item10 = new Menu()
-            {
-                Name = "Tea ",
-                Category = "",
-                Description = "",
-                price = 0
-            };
+            string item1Detail = "Stuffed with ripe avocado, lean bacon, bell pepper, a blend of Monterey Jack and Cheddar cheeses and topped with sour cream and chives. Served with our homemade salsa. ";
+            Menu item1 = new Menu("AWESOME AVOCADO", "Omelette", item1Detail, 15.10);
 
-            Menu Item11 = new Menu()
-            {
-                Name = "Orange Juice ",
-                Category = "",
-                Description = "",
-                price = 0
-            };
+            string item2Detail = "Egg whites, fresh green pepper, zucchini, mushrooms, onions, tomatoes, a blend of Monterey Jack and Cheddar cheeses and topped with chives.";
+            Menu Item2 = new Menu("VEGGIE DELIGHT", "Omelette", item2Detail, 10.30);
 
-            Menu Item12 = new Menu()
-            {
-                Name = "Coffee ",
-                Category = "",
-                Description = "",
-                price = 0
-            };
+            string item3Detail = "Egg whites, spinach, turkey sausage and diced tomato. Served with our homemade salsa.";
+            Menu Item3 = new Menu("LEAN & GREEN", "Sandwish", item3Detail, 13.00);
 
+            string item4Detail = "Seasoned chicken breast, bell peppers, onions, diced tomatoes, a blend of Monterey Jack and Cheddar cheeses and topped with sour cream and chives. Served with our homemade salsa. ";
+            Menu Item4 = new Menu("CHICKEN FAJITA ", "Omelette", item4Detail, 10.00);
+
+            string item5Detail = "This one’s got the works! Sausage, ham, bacon, bell peppers, zucchini, diced onions, mushrooms, diced tomatoes, chives, a blend of Monterey Jack and Cheddar cheeses and topped with sour cream.  ";
+            Menu Item5 = new Menu("THE ULTIMATE ", "Omelette", item5Detail, 13.50);
+
+            string item6Detail = "A helthier version of the tradisional English Breakfast: Tomato, turkey bacon, turkey ham, turkey sousage and eggs.";
+            Menu Item6 = new Menu("THE HEALTHIER ENGLISHMAN ", "Combo", item6Detail, 12.50);
+
+            string item7Detail = "Three eggs* any style, choice of tureky bacon,  chicken sausage links and served with a cup of fresh fruit and your choice of potato chunks or hash browns";
+            Menu Item7 = new Menu("THE AMERICAN", "Combo", item7Detail, 14.50);
+
+            string item8Detail = "Our traditional malted waffle topped with whipped cream and a dusting of powdered sugar. Served with a side of whipped butter and fresh seasonal fruit.";
+            Menu Item8 = new Menu("GOLDEN WAFFLE", " Pancakes", item8Detail, 11.50);
+
+            string item9Detail = "Yogourt with oats, honey and raspberry ";
+            Menu Item9 = new Menu("PARFAIT", "Yogurt", item9Detail, 5.50);
+
+            string item10Detail = "Assortments of Teas: Green, English Breakfast, Oolong, LimePie";
+            Menu Item10 = new Menu("TEA", "Beverage", item10Detail, 1.00);
+
+            string item11Detail = "Freshly squeezed orange juice";
+            Menu Item11 = new Menu("Orange Juice", "Beverage", item11Detail, 2.00);
+
+            string item12Detail = "Colombian Dark roast Coffee";
+            Menu Item12 = new Menu("Coffee", "Beverage", item12Detail, 1.25);
 
             List<Menu> menu = new List<Menu>();
 
+            // menu.Add(item0);
             menu.Add(item1);
             menu.Add(Item2);
             menu.Add(Item3);
@@ -116,27 +71,8 @@ namespace BreakfastClubMyVersion
             menu.Add(Item10);
             menu.Add(Item11);
             menu.Add(Item12);
-
             return menu;
-
-
         }
-
-
-
-        //public void PrintMenu()
-
-        //{
-        //    var items = Menu.MenuItems();
-        //    var i = 0;
-
-        //    foreach (var item in items)
-        //    {
-        //        Console.WriteLine(i+""+item);
-        //        i++;
-        //    }
-        //}
-
 
     }
 }
