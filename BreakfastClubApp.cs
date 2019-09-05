1edd11bc1de5c6a7a8854fbae241165e734a1738
LizHitches <50306247+LizHitches@users.noreply.github.com>
2019-09-05 18:14:49 -0400

    Update BreakfastClubApp.cs

--- BreakfastClubApp.cs
+++ BreakfastClubApp.cs
@@ -1,5 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Globalization;
 
 
 namespace BreakfastClub
@@ -10,47 +13,63 @@ namespace BreakfastClub
         Order order = new Order();
         int input = 0;
         string inp = "";
+        int howMany = 0;
 
         public void RunApp()
         {
             Console.WriteLine("Welcome to the Breakfast Club ");
+        
+
             bool orderMore = true;
-            while (orderMore)
-            {
-                Console.WriteLine("\n Select a Number from the Menu ");
-                Console.WriteLine("_______________________________");
-                order.PrintMenu();
-                inp = Console.ReadLine();
-                input = int.Parse(inp);
-
-                if (input > 0 && input < menus.Count)
+                while (orderMore)
                 {
-                    Console.WriteLine(menus[input].Name + "  \n Description " + menus[input].Description);
-                    Console.WriteLine("Do you want to buy this item? Y/N ");
-                    string b = Console.ReadLine().ToLower().Trim();
+                    Console.WriteLine("\n Select a Number from the Menu ");
+                    order.PrintMenu();
+                    inp = Console.ReadLine();
+                    input = int.Parse(inp);
 
-                    if (b == "y")
+                    if (input >= 0 && input <= menus.Count)
                     {
-                        if (input > 0 && input < menus.Count)
+                        Console.WriteLine(menus[input].Name + "  \n DESCRIPTION: " + menus[input].Description);
+                        Console.WriteLine("do you want to buy this item? Y/N ");
+                        string b = Console.ReadLine().ToLower().Trim();
+
+                        while (b != "n" && b != "y")
                         {
-                            Console.WriteLine("How Many?");
-                            string h = Console.ReadLine();
-                            int howMany = int.Parse(h);
-                            order.AddMenuItem(input, howMany);
-                            order.PrintCart();
-                            double a = order.GetGrandTotal();
-                            Console.WriteLine("Your total is: " + a);
+                            Console.WriteLine("invalid entry.  please enter Y or N");
+                            b = Console.ReadLine().ToLower().Trim();
+                        }
+                        if (b == "y")
+                        {
+                            if (input > 0 && input < menus.Count)
+                            {
+                                Console.WriteLine("How Many ?");
+                                string h = Console.ReadLine();
+                                try
+                                {
+                                    howMany = int.Parse(h);
+                                }
+                                catch
+                                {
+                                    Console.WriteLine("You have not enter a number. please enter number ");
+                                    h = Console.ReadLine();
+                                    howMany = int.Parse(h);
+                                }
+                                order.AddMenuItem(input, howMany);
+                                order.PrintCart();
+                                double a = order.GetGrandTotal();
+                                Console.WriteLine("your total is:" + a);
+                            }
                         }
                     }
-                }
-                else
-                {
-                    order.PrintCart();
+                    else
+                    {
+                        order.PrintCart();
 
-                    Checkout();
-                    break;
-                }
-            }
+                        Checkout();
+                        break;
+                    }
+                }         
         }
         public void Checkout()
         {
@@ -61,13 +80,13 @@ namespace BreakfastClub
             Console.WriteLine("2: Check");
             Console.WriteLine("3: Cash");
 
-            string pay = Console.ReadLine();
+            string pay = Console.ReadLine().ToLower().Trim();
 
             Payment check = new Payment();
             Payment card = new Payment();
             Payment cash = new Payment();
 
-            if (pay == "Credit/Debit" || pay == "1")
+            if (pay == "credit/debit" || pay == "1" || pay== "credit"|| pay=="debit")
             {
                 Console.WriteLine("Cradit/Debit Card Payment:");
                 card.CreditCardPaymentNumber();
@@ -90,9 +109,9 @@ namespace BreakfastClub
                 Console.WriteLine("Invalid entry please select 1-Card, 2- Check or 3- Cash ");
                 pay = Console.ReadLine();
             }
+        Console.WriteLine("Good Bye, Come back soon");
 
-            Console.WriteLine("Good Bye, Come back soon");
-        }
+    }
 
     }
 
