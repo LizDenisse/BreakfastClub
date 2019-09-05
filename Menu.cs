e414eb135afaaf477c1726b44af7847a9b89e5d2
LizHitches <50306247+LizHitches@users.noreply.github.com>
2019-09-05 15:22:32 -0400

    Update Menu.cs

--- Menu.cs
+++ Menu.cs
@@ -20,41 +20,41 @@ namespace BreakfastClub
         static public List<Menu> MenuItems()
         {
 
-            string item1Detail = "Eggs and Cheese Ommelette with avocado, red peppers and turkey bacon  ";
-            Menu item1 = new Menu("Awesome Avocado", "Omellet", item1Detail, 15.10);
+            string item1Detail = "Stuffed with ripe avocado, lean bacon, bell pepper, a blend of Monterey Jack and Cheddar cheeses and topped with sour cream and chives. Served with our homemade salsa. ";
+            Menu item1 = new Menu("AWESOME AVOCADO", "Omelette", item1Detail, 15.10);
 
-            string item2Detail = "Eggs and Cheese Ommelette with only ";
-            Menu Item2 = new Menu("Veggie Delight  ", "Omellet", item2Detail, 10.30);
+            string item2Detail = "Egg whites, fresh green pepper, zucchini, mushrooms, onions, tomatoes, a blend of Monterey Jack and Cheddar cheeses and topped with chives.";
+            Menu Item2 = new Menu("VEGGIE DELIGHT", "Omelette", item2Detail, 10.30);
 
-            string item3Detail = "Eggs and Cheese Ommelette";
-            Menu Item3 = new Menu("LEAN & GREEN", "Omellet", item3Detail, 13.00);
+            string item3Detail = "Egg whites, spinach, turkey sausage and diced tomato. Served with our homemade salsa.";
+            Menu Item3 = new Menu("LEAN & GREEN", "Sandwish", item3Detail, 13.00);
 
-            string item4Detail = "Eggs and Cheese Ommelette";
-            Menu Item4 = new Menu("CHICKEN FAJITA ", "Fajita", item4Detail, 10.00);
+            string item4Detail = "Seasoned chicken breast, bell peppers, onions, diced tomatoes, a blend of Monterey Jack and Cheddar cheeses and topped with sour cream and chives. Served with our homemade salsa. ";
+            Menu Item4 = new Menu("CHICKEN FAJITA ", "Omelette", item4Detail, 10.00);
 
-            string item5Detail = "Eggs and Cheese Ommelette";
-            Menu Item5 = new Menu("Jump Start  Sanwish ", "Sandwhich", item5Detail, 13.50);
+            string item5Detail = "This one’s got the works! Sausage, ham, bacon, bell peppers, zucchini, diced onions, mushrooms, diced tomatoes, chives, a blend of Monterey Jack and Cheddar cheeses and topped with sour cream.  ";
+            Menu Item5 = new Menu("THE ULTIMATE ", "Omelette", item5Detail, 13.50);
 
-            string item6Detail = "Eggs and Cheese Ommelette";
-            Menu Item6 = new Menu("The healthier English", "Combo", item6Detail, 12.50);
+            string item6Detail = "A helthier version of the tradisional English Breakfast: Tomato, turkey bacon, turkey ham, turkey sousage and eggs.";
+            Menu Item6 = new Menu("THE HEALTHIER ENGLISHMAN ", "Combo", item6Detail, 12.50);
 
-            string item7Detail = "Eggs and Cheese Ommelette";
-            Menu Item7 = new Menu("The happy American", "Snadwhich", item7Detail, 14.50);
+            string item7Detail = "Three eggs* any style, choice of tureky bacon,  chicken sausage links and served with a cup of fresh fruit and your choice of potato chunks or hash browns";
+            Menu Item7 = new Menu("THE AMERICAN", "Combo", item7Detail, 14.50);
 
-            string item8Detail = "Eggs and Cheese Ommelette";
-            Menu Item8 = new Menu("Buttermilk Pancakes", " Pancakes", item8Detail, 11.50);
+            string item8Detail = "Our traditional malted waffle topped with whipped cream and a dusting of powdered sugar. Served with a side of whipped butter and fresh seasonal fruit.";
+            Menu Item8 = new Menu("GOLDEN WAFFLE", " Pancakes", item8Detail, 11.50);
 
-            string item9Detail = "Eggs and Cheese Ommelette";
-            Menu Item9 = new Menu("Parfait", "Yogurt", item9Detail, 5.50);
+            string item9Detail = "Yogourt with oats, honey and raspberry ";
+            Menu Item9 = new Menu("PARFAIT", "Yogurt", item9Detail, 5.50);
 
-            string item10Detail = "Eggs and Cheese Ommelette";
-            Menu Item10 = new Menu("Tea", "Hot Tea", item10Detail, 1.00);
+            string item10Detail = "Assortments of Teas: Green, English Breakfast, Oolong, LimePie";
+            Menu Item10 = new Menu("TEA", "Beverage", item10Detail, 1.00);
 
-            string item11Detail = "Eggs and Cheese Ommelette";
-            Menu Item11 = new Menu("Orange Juice", "Cold Orange Juice", item11Detail, 2.00);
+            string item11Detail = "Freshly squeezed orange juice";
+            Menu Item11 = new Menu("Orange Juice", "Beverage", item11Detail, 2.00);
 
-            string item12Detail = "Eggs and Cheese Ommelette";
-            Menu Item12 = new Menu("Coffee", "Dark Roasted Coffee", item12Detail, 1.25);
+            string item12Detail = "Colombian Dark roast Coffee";
+            Menu Item12 = new Menu("Coffee", "Beverage", item12Detail, 1.25);
 
             List<Menu> menu = new List<Menu>();
 
